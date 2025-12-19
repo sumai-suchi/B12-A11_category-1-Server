6f66bf5ce68dc8229f02be5362b961ccff8e6e7d
sumaiya <sumaiyamoina@gmail.com>
2025-12-19 19:31:43 +0600

    added server side with mongodb connection and created a api for posting user information and a get api to get the information with email

---
+++ index.js
@@ -0,0 +1,63 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cgi21.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("BloodDonationDB");
+    const userCollection = database.collection("user");
+
+    app.post("/user", async (req, res) => {
+      const userInfo = req.body;
+      userInfo.role = "donor";
+      userInfo.createdAt = new Date();
+
+      const result = await userCollection.insertOne(userInfo);
+
+      res.send(result);
+    });
+
+    app.get("/user/role/:email", async (req, res) => {
+      const { email } = req.params;
+      console.log(email);
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("mongoDb is running on 500");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on this port ${port}`);
+});
