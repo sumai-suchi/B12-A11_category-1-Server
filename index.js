38b48e3245b6f29806b2239921940916a544076e
sumaiya <sumaiyamoina@gmail.com>
2025-12-22 05:11:59 +0600

    added three api for get a single request information and to update it or edit it also get all request api for admin

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -76,6 +76,86 @@ async function run() {
       const result = await userCollection.findOne(query);
       res.send(result);
     });
+
+    app.get("/my-donation-request", verifyFBToken, async (req, res) => {
+      const { limit } = req.query;
+      const email = req.decoded_email;
+      console.log(email);
+      const query = { requesterEmail: email };
+      const limitNum = Number(limit) || 0;
+      console.log(limitNum);
+      console.log(limit);
+      const result = await bloodDonationRequest
+        .find(query)
+        .limit(limitNum)
+        .toArray();
+      return res.send(result);
+    });
+
+    app.patch("/update/user/status", verifyFBToken, async (req, res) => {
+      const { email, status } = req.query;
+      const query = { email: email };
+      const updateStatus = {
+        $set: {
+          status: status,
+        },
+      };
+
+      const result = await userCollection.updateOne(query, updateStatus);
+      return res.send(result);
+    });
+    app.patch("/update/userRequest/status", verifyFBToken, async (req, res) => {
+      const { donationStatus, id } = req.query;
+      console.log(donationStatus, id);
+      const query = { _id: new ObjectId(id) };
+      const updateStatus = {
+        $set: {
+          donationStatus: donationStatus,
+        },
+      };
+
+      const result = await bloodDonationRequest.updateOne(query, updateStatus);
+      return res.send(result);
+    });
+
+    app.get("/userRequest/:id", verifyFBToken, async (req, res) => {
+      const { id } = req.params;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await bloodDonationRequest.findOne(query);
+      res.send(result);
+    });
+
+    app.patch(
+      "/update/singleUserRequest/:id",
+      verifyFBToken,
+      async (req, res) => {
+        const { id } = req.params;
+        console.log(id);
+        const query = { _id: new ObjectId(id) };
+        const userInfo = req.body;
+        console.log(userInfo);
+        const UpdateOne = {
+          $set: {
+            requesterName: userInfo.requesterName,
+            requesterEmail: userInfo.requesterEmail,
+            recipientName: userInfo.recipientName,
+            district: userInfo.district,
+            upazila: userInfo.upazila,
+            hospitalName: userInfo.hospitalName,
+            address: userInfo.address,
+            bloodGroup: userInfo.bloodGroup,
+            donationDate: userInfo.donationDate,
+            donationTime: userInfo.donationTime,
+            requestMessage: userInfo.requestMessage,
+            donationStatus: userInfo.donationStatus,
+          },
+        };
+        const result = await bloodDonationRequest.updateOne(query, UpdateOne);
+        console.log(userInfo);
+        res.send(result);
+      }
+    );
 
     app.post("/blood-donation-request", verifyFBToken, async (req, res) => {
       const DonationRequesterInfo = req.body;
